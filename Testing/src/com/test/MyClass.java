aa06e1508483de68985479b8cf8fca59b7383ff3
pedromsen@hotmail.com <pedromsen@hotmail.com>
2012-10-27 12:31:19 +0200

    Project added

---
+++ Testing/src/com/test/MyClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.test;
+
+/**
+ * @author aalonso
+ *
+ */
+public class MyClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+			System.out.println("Helllo World");
+	}
+
+}
